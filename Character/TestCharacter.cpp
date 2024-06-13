d81108a3c6698b56f8c365e93f2c1d4ef02bb795
CharlieJJ0115 <shangyuchiayi@gmail.com>
2024-06-13 21:30:07 +0800

    ADD two character file

---
+++ Character/TestCharacter.cpp
@@ -0,0 +1,7 @@
+#include <string>
+
+#include "TestCharacter.hpp"
+
+TestCharacter::TestCharacter(int x, int y) : Character("play/enemy-1.png", x, y, 10, 50, 5, 5) {
+	// TODO: [CUSTOM-TOOL] You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
+}
---
+++ Character/TestCharacter.hpp
@@ -0,0 +1,9 @@
+#ifndef TESTCHARACTER_HPP
+#define TESTCHARACTER_HPP
+#include "Character.hpp"
+
+class TestCharacter : public Character {
+public:
+	TestCharacter(int x, int y);
+};
+#endif // TESTCHARACTER_HPP
